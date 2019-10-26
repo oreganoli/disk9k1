7ee85ee0d7666c1b3932162fca7f23e03919cc70
oreganoli <3611916+oreganoli@users.noreply.github.com>
2019-10-26 19:49:45 +0200

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> String {
+    "Hello world!".to_owned()
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![index])
+        .launch();
 }
