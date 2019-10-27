fab72c60de755947db5a134b89a4b9a50b5f8db7
oreganoli <3611916+oreganoli@users.noreply.github.com>
2019-10-27 21:48:04 +0100

    server now holds the data in memory and spits it out back at user

--- src/upload.rs
+++ src/upload.rs
@@ -1,20 +1,26 @@
 use rocket::Data;
 use rocket::http::ContentType;
-use rocket_multipart_form_data::{FileField, mime, MultipartFormData, MultipartFormDataField, MultipartFormDataOptions};
+use rocket_multipart_form_data::{FileField, mime, MultipartFormData, MultipartFormDataField, MultipartFormDataOptions, RawField};
 
 use crate::prelude::*;
 
 #[post("/upload", data = "<data>")]
 pub fn upload(content_type: &ContentType, data: Data) -> Html<String> {
     let mut options = MultipartFormDataOptions::new();
-    options.allowed_fields.push(MultipartFormDataField::file("file"));
+    options.allowed_fields.push(MultipartFormDataField::raw("file"));
     let mut form_data = MultipartFormData::parse(content_type, data, options).unwrap();
-    let x = form_data.files.remove("file");
+    let x = form_data.raw.remove("file");
     match x {
         Some(f) => {
             match f {
-                FileField::Single(g) => {
-                    Html(format!("<p>Filename: {:?}</p><p>Content type: {:?}</p><p>Path: {:?}</p>", g.file_name, g.content_type, g.path))
+                RawField::Single(f) => {
+                    Html(
+                        format!("<p>Filename: {:?}</p><p>Content type: {:?}</p><p>Data:</p><pre>{}</pre>",
+                                f.file_name,
+                                f.content_type,
+                                std::str::from_utf8(&f.raw).unwrap()
+                        )
+                    )
                 }
                 _ => Html(format!("<p>Upload failed</p>"))
             }
