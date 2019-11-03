8758ede7695b6a5014bd9f29a39a9168b03f297b
oreganoli <3611916+oreganoli@users.noreply.github.com>
2019-11-03 19:19:26 +0100

    Added a fallback template for template rendering errors

--- src/util.rs
+++ src/util.rs
@@ -5,6 +5,8 @@ pub struct Renderer(pub Tera);
 
 impl Renderer {
     pub fn html(&self, name: &str, ctx: &Context) -> Page {
-        Html(self.0.render(name, ctx).unwrap())
+        Html(self.0.render(name, ctx).unwrap_or(
+            self.0.render("template_error.html", &Context::new()).unwrap()
+        ))
     }
 }
