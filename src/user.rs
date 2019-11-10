2ab59d2ea6052d2e1af1593f9f54a5d7a0763f5a
oreganoli <3611916+oreganoli@users.noreply.github.com>
2019-11-10 20:28:07 +0100

    User insertable constructor and a converter to UserInfo
    Signed-off-by: oreganoli <1141291-oreganoli@users.noreply.gitlab.com>

--- src/user.rs
+++ src/user.rs
@@ -4,9 +4,9 @@ use crate::prelude::*;
 
 /// The publicly-visible information about users.
 #[derive(Serialize)]
-pub struct UserInfo {
+pub struct UserInfo<'a> {
     pub id: i32,
-    pub name: String,
+    pub name: &'a str,
     pub joined: NaiveDateTime,
     pub is_admin: bool,
 }
@@ -21,4 +21,28 @@ struct User {
     quick_token: String,
     joined: Option<NaiveDateTime>,
     is_admin: bool,
+}
+
+impl User {
+    /// A constructor lacking the `id` and `joined` fields, which the DB should supply by itself.
+    pub fn new(name: String, password: String, quick_token: String, is_admin: bool) -> Self {
+        User {
+            id: None,
+            name,
+            password,
+            quick_token,
+            joined: None,
+            is_admin,
+        }
+    }
+    fn to_info(&self) -> UserInfo {
+        UserInfo {
+            id: self.id.expect("Could not get an ID out of the User struct"),
+            name: &self.name,
+            joined: self
+                .joined
+                .expect("Could not get a joining date out of the User struct"),
+            is_admin: self.is_admin,
+        }
+    }
 }
