54f23e88f7e625821fcfa188295f7c0f9f00aa1e
oreganoli <3611916+oreganoli@users.noreply.github.com>
2019-11-10 17:17:23 +0100

    made the template-error-template more informative
    Signed-off-by: oreganoli <1141291-oreganoli@users.noreply.gitlab.com>

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 use crate::prelude::*;
 
 /// A newtype that wraps `Tera` so we can avoid writing the `Html` wrapper and `.unwrap()`ping the value over and over.
@@ -5,16 +7,22 @@ pub struct Renderer(pub Tera);
 
 impl Renderer {
     pub fn html(&self, name: &str, ctx: &Context) -> Page {
-        Html(
-            self.0.render(name, ctx).unwrap_or(
-                self.0
-                    .render("template_error.html", &Context::new())
-                    .unwrap(),
-            ),
-        )
+        Html(self.0.render(name, ctx).unwrap_or_else(|f| {
+            let string = format!("Error description: {}, kind: {:?}", f, f.source());
+            let mut ctx = Context::new();
+            ctx.insert("reason", &string);
+            self.0.render("template_error.html", &ctx).unwrap()
+        }))
     }
 }
 
 pub fn mebibytes(bytes: u64) -> f64 {
     bytes as f64 / BYTES_TO_MEBIBYTE
+}
+
+/// Creates a `Pool` pointing at our database.
+pub fn create_pool() -> Pool {
+    let url = std::env::var("DATABASE_URL").expect("DATABASE_URL is not set, panicking.");
+    let manager = Manager::new(url);
+    Pool::builder().max_size(8).build(manager).unwrap()
 }
