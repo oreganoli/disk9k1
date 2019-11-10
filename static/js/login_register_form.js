ec709b12f4c8823429508c9e822fc0aea6825733
oreganoli <3611916+oreganoli@users.noreply.github.com>
2019-11-10 20:00:12 +0100

    adjustments to the form flexboxes
    Signed-off-by: oreganoli <1141291-oreganoli@users.noreply.gitlab.com>

--- static/js/login_register_form.js
+++ static/js/login_register_form.js
@@ -3,14 +3,19 @@ $(document).ready(function () {
     let reg_form = $(".registration_form");
     let login_button = $(".login");
     let reg_button = $(".register");
+    let or = $(".or");
     login_form.hide();
     reg_form.hide();
     login_button.click(function () {
+        or.hide();
         login_form.show();
         login_button.hide();
+        reg_button.hide();
     });
     reg_button.click(function () {
+        or.hide();
         reg_form.show();
         reg_button.hide();
+        login_button.hide();
     });
 });
