3a32655d6ccf85c51da95bac592183f0c4c668fd
oreganoli <3611916+oreganoli@users.noreply.github.com>
2019-11-10 14:50:29 +0100

    added type aliases for connection-related types
    Signed-off-by: oreganoli <1141291-oreganoli@users.noreply.gitlab.com>

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,3 +1,4 @@
+use diesel::PgConnection;
 pub use rocket::{response::content::Html, State};
 pub use serde::{Deserialize, Serialize};
 pub use tera::{Context, Tera};
@@ -13,3 +14,10 @@ pub type TeraState<'a> = State<'a, Renderer>;
 pub type Page = Html<String>;
 
 pub const BYTES_TO_MEBIBYTE: f64 = 1048576.;
+
+/// Shorthand alias for a pooled `r2d2` connection.
+pub type Connection = diesel::r2d2::PooledConnection<Manager>;
+/// Shorthand alias for a Postgres connection manager provided by `diesel::r2d2`.
+pub type Manager = diesel::r2d2::ConnectionManager<PgConnection>;
+/// Shorthand alias for a `diesel::r2d2::Pool`.
+pub type Pool = diesel::r2d2::Pool<Manager>;
