d25ab9b163f5c08a9a846e17ddb8329f7b8d4975
oreganoli <3611916+oreganoli@users.noreply.github.com>
2019-11-24 18:44:19 +0100

    f-ing finally figured out why the instance settings form wasn't loading
    Signed-off-by: oreganoli <1141291-oreganoli@users.noreply.gitlab.com>

--- js/src/controllers/modify_instance.js
+++ js/src/controllers/modify_instance.js
@@ -17,6 +17,13 @@ const ModifyInstanceForm = {
             Instance.load();
         })
     },
+    oncreate: () => {
+        Instance.load_then(() => {
+            document.getElementById("inst_text_input").setAttribute("value", Instance.name);
+            document.getElementById("inst_desc_input").innerText = Instance.description;
+            document.getElementById("inst_size_input").setAttribute("value", Instance.size_limit);
+        })
+    },
     view: () => {
         return m("form", {
             onsubmit: (e) => {
@@ -25,21 +32,19 @@ const ModifyInstanceForm = {
             }
         }, [
             m("label", "Instance name"),
-            m("input[type=text]", {
+            m("input#inst_text_input[type=text]", {
                 oninput: (e) => {
                     ModifyInstanceForm.data.name = e.target.value;
-                },
-                value: Instance.name
+                }
             }),
             m("label", "Description"),
-            m("textarea", {
+            m("textarea#inst_desc_input", {
                 oninput: (e) => {
                     ModifyInstanceForm.data.description = e.target.value;
-                },
-                value: Instance.description
+                }
             }),
             m("label", "Size limit (B)"),
-            m("input[type=number]", {
+            m("input#inst_size_input[type=number]", {
                 oninput: (e) => {
                     ModifyInstanceForm.data.description = e.target.value;
                 },
--- js/src/models/instance.js
+++ js/src/models/instance.js
@@ -14,6 +14,18 @@ const Instance = {
             Instance.description = result.description;
             Instance.size_limit = result.size_limit;
         });
+    },
+    load_then: (func) => {
+        m.request({
+            method: "GET",
+            url: "/instance",
+            withCredentials: false
+        }).then((result) => {
+            Instance.name = result.name;
+            Instance.description = result.description;
+            Instance.size_limit = result.size_limit;
+            func();
+        });
     }
 };
 module.exports = Instance;
