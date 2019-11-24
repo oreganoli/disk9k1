5b6fc217ec785c50326a4a10949a504e3d20e9c7
oreganoli <3611916+oreganoli@users.noreply.github.com>
2019-11-24 20:16:15 +0100

    users can now change their passwords
    Signed-off-by: oreganoli <1141291-oreganoli@users.noreply.gitlab.com>

--- js/src/views/me.js
+++ js/src/views/me.js
@@ -1,6 +1,6 @@
 const m = require("mithril");
 const CurrentUser = require("../models/current_user");
-
+const ChangePasswordForm = require("../controllers/change_password_form");
 const MeView = {
     view: () => (
         m(".user_profile", [
@@ -10,7 +10,9 @@ const MeView = {
                 m("tr", [m("th.hor", "Username"), m("td", CurrentUser.user.name)]),
                 m("tr", [m("th.hor", "Email"), m("td", m("a", {href: `mailto:${CurrentUser.user.email}`}, CurrentUser.user.email))]),
                 m("tr", [m("th.hor", "Joined"), m("td", CurrentUser.user.joined)]),
-                m("tr", [m("th.hor", "Role"), m("td", CurrentUser.user.is_admin ? "Admin" : "User")])])
+                m("tr", [m("th.hor", "Role"), m("td", CurrentUser.user.is_admin ? "Admin" : "User")])]
+            ),
+            m(ChangePasswordForm)
             ])
     )
 };
