296d06d6860037f94ffd59a261eb8b133b9ee641
oreganoli <3611916+oreganoli@users.noreply.github.com>
2019-11-25 13:53:01 +0100

    child directories are wrapped in a "directories" key
    Signed-off-by: oreganoli <1141291-oreganoli@users.noreply.gitlab.com>

--- src/directory/read.rs
+++ src/directory/read.rs
@@ -2,12 +2,24 @@ use crate::prelude::*;
 
 use super::Directory;
 
+#[derive(Serialize)]
+pub struct DirectoryView {
+    directories: Vec<Directory>,
+}
+
+impl DirectoryView {
+    fn from_vec(vec: Vec<Directory>) -> Self {
+        Self { directories: vec }
+    }
+}
+
 #[get("/drive")]
-pub fn top_for_user(app: AppState, mut cookies: Cookies) -> AppResult<Json<Vec<Directory>>> {
+pub fn top_for_user(app: AppState, mut cookies: Cookies) -> AppResult<Json<DirectoryView>> {
     let inst = app.read();
     let user = match inst.user_from_cookies(&mut cookies) {
         Some(u) => u,
         None => return AppError::user_auth(AuthError::Unauthenticated),
     };
-    Ok(Json(inst.dir_repo.read_top_for_user(&user)?))
+    let dir_view = DirectoryView::from_vec(inst.dir_repo.read_top_for_user(&user)?);
+    Ok(Json(dir_view))
 }
