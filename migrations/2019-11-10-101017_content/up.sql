15cf7c0e8ca35de9bea4b70e9288dc4a014eaf0b
oreganoli <3611916+oreganoli@users.noreply.github.com>
2019-12-02 22:51:23 +0100

    changed SERIAL to INTEGER, made content uploads default to 1
    Signed-off-by: oreganoli <1141291-oreganoli@users.noreply.gitlab.com>

--- migrations/2019-11-10-101017_content/up.sql
+++ migrations/2019-11-10-101017_content/up.sql
@@ -2,7 +2,7 @@ CREATE TABLE content(
                         hash             INTEGER PRIMARY KEY,
                         mimetype         TEXT      NOT NULL,
                         first_uploaded   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                        times_uploaded   INTEGER   NOT NULL DEFAULT 0,
+                        times_uploaded   INTEGER   NOT NULL DEFAULT 1,
                         times_downloaded INTEGER   NOT NULL DEFAULT 0,
                         data             BYTEA     NOT NULL
 );
--- migrations/2019-11-10-101026_files/up.sql
+++ migrations/2019-11-10-101026_files/up.sql
@@ -1,9 +1,9 @@
-CREATE TABLE files (
-  id SERIAL PRIMARY KEY,
-  filename TEXT NOT NULL,
-  hash SERIAL NOT NULL REFERENCES content(hash),
-  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  directory SERIAL NOT NULL REFERENCES directories(id),
-  public BOOLEAN NOT NULL
+CREATE TABLE files(
+                      id        SERIAL PRIMARY KEY,
+                      filename  TEXT      NOT NULL,
+                      hash      SERIAL    NOT NULL REFERENCES content (hash),
+                      created   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                      updated   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                      directory INTEGER   NOT NULL REFERENCES directories (id),
+                      public    BOOLEAN   NOT NULL
 );
