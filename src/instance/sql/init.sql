c5af7ef8560c42f774294dc4c923693cb902125c
oreganoli <3611916+oreganoli@users.noreply.github.com>
2019-12-29 22:04:24 +0100

    added a constraint to the instance table
    Signed-off-by: oreganoli <1141291-oreganoli@users.noreply.gitlab.com>

--- src/instance/sql/init.sql
+++ src/instance/sql/init.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS instance
 (
     name        TEXT NOT NULL PRIMARY KEY,
     description TEXT,
-    size_limit  BIGINT
+    size_limit BIGINT NOT NULL CHECK (size_limit >= 0)
 );
