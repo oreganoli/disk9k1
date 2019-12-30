c58842788f62b200dce6052e0dcab9805b6c11aa
oreganoli <3611916+oreganoli@users.noreply.github.com>
2019-12-30 15:12:48 +0100

    simplified error wrappers
    Signed-off-by: oreganoli <1141291-oreganoli@users.noreply.gitlab.com>

--- src/error.rs
+++ src/error.rs
@@ -14,7 +14,7 @@ impl<'a> From<r2d2::Error> for ErrorWrapper {
     fn from(e: r2d2::Error) -> Self {
         Self {
             status: Status::InternalServerError,
-            name: e.name().unwrap_or("Unknown internal error").to_owned(),
+            name: e.to_string(),
         }
     }
 }
@@ -31,6 +31,6 @@ impl<'a> From<postgres::Error> for ErrorWrapper {
 impl<'r> Responder<'r> for ErrorWrapper {
     fn respond_to(self, request: &Request<'_>) -> rocket::response::Result<'r> {
         use rocket::response::status::Custom;
-        Custom(self.status, Json(self.name)).respond_to(request)
+        Custom(self.status, Json(self)).respond_to(request)
     }
 }
