79852215bc3d7a8fab7b6766af050ebe0bd6b8a8
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-01-05 16:23:11 +0100

    remove Mithril
    Signed-off-by: oreganoli <1141291-oreganoli@users.noreply.gitlab.com>

--- js/src/views/login.js
+++
@@ -1,9 +0,0 @@
-const m = require("mithril");
-const LoginForm = require("../controllers/login_form");
-const RegisterForm = require("../controllers/register_form");
-const LoginView = {
-    view: () => {
-        return [m(LoginForm), m("h2", "or"), m(RegisterForm)];
-    }
-};
-module.exports = LoginView;
--- js/src/views/panel.js
+++
@@ -1,9 +0,0 @@
-const m = require("mithril");
-const ModifyInstanceForm = require("../controllers/modify_instance");
-
-const Panel = {
-    view: () => {
-        return m(ModifyInstanceForm);
-    }
-};
-module.exports = Panel;
