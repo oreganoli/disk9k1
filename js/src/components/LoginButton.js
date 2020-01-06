4dc813f9c502d970b968eee97bd820d50f96e766
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-01-06 13:26:03 +0100

    made login button redirect to /login route
    Signed-off-by: oreganoli <1141291-oreganoli@users.noreply.gitlab.com>

--- js/src/components/LoginButton.js
+++ js/src/components/LoginButton.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import {Redirect} from "react-router";
 
 class LoginButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            toLogin: false
+        };
+    }
+
     render() {
-        return <button>Sign in</button>;
+        if (!this.state.toLogin) {
+            return <button onClick={() => {
+                this.setState(() => ({toLogin: true}))
+            }}>Sign in</button>;
+        } else {
+            return <div>
+                <button>Sign in</button>
+                <Redirect to={"/login"}/></div>;
+        }
     }
 }
 
