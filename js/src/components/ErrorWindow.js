173d5b38902b9048ea603f38ec917b409d914d76
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-01-06 01:27:14 +0100

    created a modal window for errors
    Signed-off-by: oreganoli <1141291-oreganoli@users.noreply.gitlab.com>

--- js/src/components/ErrorWindow.js
+++ js/src/components/ErrorWindow.js
@@ -4,7 +4,17 @@ import {connect} from 'react-redux';
 class ErrorWindow extends React.Component {
     render() {
         if (this.props.error != null) {
-            return <div className={"modal_overlay"}/>;
+            return <div className={"overlay"}>
+                <div className={"modal_overlay"}/>
+                <div className={"modal_window"}>
+                    <h1 className={"error_reason"}>Error</h1>
+                    <h2 className={"error_reason"}>{this.props.error.name}</h2>
+                    <button className={"central centeredButton"} onClick={() => {
+                        this.props.acceptError();
+                    }}>OK
+                    </button>
+                </div>
+            </div>;
         } else {
             return <div/>
         }
@@ -14,5 +24,5 @@ class ErrorWindow extends React.Component {
 const mapStateToProps = (state) => ({
     error: state.error
 });
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => ({acceptError: () => dispatch({type: "SET_ERROR", payload: null})});
 export default connect(mapStateToProps, mapDispatchToProps)(ErrorWindow);
